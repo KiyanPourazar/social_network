befb4249c3bdf091bbe3dccb384fe727920fd7ff
Kiyan Pourazar <74766125+KiyanPourazar@users.noreply.github.com>
2025-09-23 09:33:05 +0330

    token and urls

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,13 +10,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
-    password_1 = serializers.CharField(required=True)
-    password_2 = serializers.CharField(required=True)
+    password_1 = serializers.CharField(required=True, write_only=True)
+    password_2 = serializers.CharField(required=True, write_only=True)
 
     class Meta:
         model = User
         fields = (
-            'email','username' ,'password_1', 'password_2',
+            'id' ,'email','username' ,'password_1', 'password_2',
             'first_name', 'last_name',
         )
         extra_kwargs = {
@@ -25,18 +25,18 @@ class RegisterSerializer(serializers.ModelSerializer):
         }
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password_2']:
+        if attrs['password_1'] != attrs['password_2']:
             raise serializers.ValidationError(
                 {'password_2': 'Passwords did not match'}
             )
-        return attrs
+        return super(RegisterSerializer, self).validate(attrs)
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data.get['first_name'],
-            last_name=validated_data.get['last_name'],
+            first_name=validated_data.get('first_name'),
+            last_name=validated_data.get('last_name'),
+            password=validated_data['password_1'],
         )
-        user.set_password(validated_data['password_1'])
-        user.save()
+        return user
